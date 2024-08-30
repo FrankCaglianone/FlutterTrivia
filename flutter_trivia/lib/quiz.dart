9c4e4ef1e84664cf8ad73012b0e905775ab27491
Francesco Caglianone <francescocaglianone@gmail.com>
2024-08-30 11:08:49 +0200

    switchScreen is now working

--- flutter_trivia/lib/quiz.dart
+++ flutter_trivia/lib/quiz.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_trivia/questions_screen.dart';
 import 'package:flutter_trivia/start_screen.dart';
-
-
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -13,6 +12,20 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  Widget? activeScreen;
+
+  @override
+  void initState() {
+    activeScreen = StartScreen(switchScreen);
+    super.initState();
+  }
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,7 +34,7 @@ class _QuizState extends State<Quiz> {
           decoration: const BoxDecoration(
             color: Color.fromARGB(255, 101, 2, 147),
           ),
-          child: const StartScreen(),
+          child: activeScreen,
         ),
       ),
     );
