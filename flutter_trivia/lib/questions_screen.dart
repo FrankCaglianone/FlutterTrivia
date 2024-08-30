ed8f539b2c1675f9e8de5576d571401ba6307976
Francesco Caglianone <francescocaglianone@gmail.com>
2024-08-30 12:21:38 +0200

    created the answer button screen

--- flutter_trivia/lib/questions_screen.dart
+++ flutter_trivia/lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_trivia/widgets/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -9,10 +10,29 @@ class QuestionsScreen extends StatefulWidget {
   }
 }
 
-
 class _QuestionScreen extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text("data");
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          // Question
+          const Text("Question", 
+            style: TextStyle(
+              color: Colors.white),
+          ),
+
+          // Spacer
+          const SizedBox(height: 50),
+
+          // Answers
+          AnswerButton("answerText", () { }),
+          AnswerButton("answerText", () { }),
+          AnswerButton("answerText", () { }),
+          AnswerButton("answerText", () { }),
+        ],
+      ),
+    );
   }
 }
